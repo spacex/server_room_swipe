c5691c10d1018c1f4044126486229ce6f3c8e889
Chris Browning <cbrowning@si.lan>
2019-09-09 16:12:13 -0500

    initial commit

---
+++ app/models.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    role = db.Column(db.Integer, default=0)
+    email = db.Column(db.String(120), index=True, unique=True)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+class Badge(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userid = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class Scan(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    badge_id = db.Column(db.Integer, db.ForeignKey('badge.id'))
+
+
+
