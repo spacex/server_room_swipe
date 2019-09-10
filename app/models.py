fb79787a5ee93c706eade94ab96b83a7bd367fc1
Chris Browning <cbrowning@si.lan>
2019-09-10 13:57:59 -0500

    now only admin users can add other users

--- app/models.py
+++ app/models.py
@@ -11,7 +11,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    role = db.Column(db.Integer, default=0)
+    is_admin = db.Column(db.Boolean)
     email = db.Column(db.String(120), index=True, unique=True)
 
     def __repr__(self):
