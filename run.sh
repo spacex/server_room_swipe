7f58f2d16c62cc6b2dbb9d10b9f4637df8b585f5
Chris Browning <cbrowning@si.lan>
2019-09-13 14:49:36 -0500

    badge reader needs to wait for server to come up try to get a token

--- run.sh
+++ run.sh
@@ -5,18 +5,21 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 cd /home/pi/server_room_swipe
-echo "Starting badge reader"
-python pi_client.py &
-pi_client_pid=$!
-echo "Started badge reader on PID: $pi_client_pid"
-
 export FLASK_APP=server_room_swipe.py
 echo "#############################################################################"
-echo "Starting flask, wait for \"Running on http://...\" before scanning any badges"
+echo "Starting flask, badge reader will come up in ~30seconds"
 echo "#############################################################################"
 flask run --port=80 --host=0.0.0.0 &
 flask_pid=$!
 echo "Started flask on PID: $flask_pid"
+
+sleep 30
+# the badge reader tries to immediately get a token
+# which won't work until the server gets fully up (~30s)
+echo "Starting badge reader"
+python pi_client.py &
+pi_client_pid=$!
+echo "Started badge reader on PID: $pi_client_pid"
 
 echo $pi_client_pid >server_room_swipe.pids
 echo $flask_pid >>server_room_swipe.pids
