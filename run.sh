3d3e0a3a560ec6312c78366b253a66fb3ce9dd98
Chris Browning <cbrowning@si.lan>
2019-09-13 11:48:43 -0500

    set server to run on port 80

--- run.sh
+++ run.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
 cd /home/pi/server_room_swipe
 export FLASK_APP=server_room_swipe.py
 
-flask run --host=0.0.0.0 &
+flask run --port=80 --host=0.0.0.0 &
 python pi_client.py &
 
