44a7cfa38614a15590cede0b02390dd69dd1de6f
Michael Williams <mspacex@gmail.com>
2019-09-28 06:33:02 +0100

    Change run.sh to work properly.

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+pushd $(dirname $0) &> /dev/null
+
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root"
    exit 1
 fi
-cd /home/pi/server_room_swipe
+
 export FLASK_APP=server_room_swipe.py
 echo "#############################################################################"
 echo "Starting flask, badge reader will come up in ~30seconds"
@@ -17,9 +19,11 @@ sleep 30
 # the badge reader tries to immediately get a token
 # which won't work until the server gets fully up (~30s)
 echo "Starting badge reader"
-python pi_client.py &
+python pi_client.py &> server.log &
 pi_client_pid=$!
 echo "Started badge reader on PID: $pi_client_pid"
 
 echo $pi_client_pid >server_room_swipe.pids
 echo $flask_pid >>server_room_swipe.pids
+
+popd &> /dev/null
